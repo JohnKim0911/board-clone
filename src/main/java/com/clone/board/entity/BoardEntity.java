92379ce7d8fe2105256087a446d4f558599aef5d
John Kim <john.donggyu.kim@gmail.com>
2025-03-23 14:59:49 +0900

    [06_게시글 수정(Board Update)]완료

---
+++ src/main/java/com/clone/board/entity/BoardEntity.java
@@ -0,0 +1,42 @@
+package com.clone.board.entity;
+
+import com.clone.board.dto.BoardDTO;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "board_table")
+public class BoardEntity extends BaseEntity {
+    @Id //pk 컬럼 지정. 필수
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //mysql - auto_increment
+    private Long id;
+
+    @Column(length = 20, nullable = false)
+    private String boardWriter;
+
+//    @Column(unique = true)
+    @Column //크기255, null 가능
+    private String boardPass;
+
+    @Column
+    private String boardTitle;
+
+    @Column(length = 500)
+    private String boardContents;
+
+    @Column
+    private int boardHits;
+
+    public static BoardEntity toSaveEntity(BoardDTO boardDTO) {
+        BoardEntity boardEntity = new BoardEntity();
+        boardEntity.setBoardWriter(boardDTO.getBoardWriter());
+        boardEntity.setBoardPass(boardDTO.getBoardPass());
+        boardEntity.setBoardTitle(boardDTO.getBoardTitle());
+        boardEntity.setBoardContents(boardDTO.getBoardContents());
+        boardEntity.setBoardHits(0);
+        return boardEntity;
+    }
+}
